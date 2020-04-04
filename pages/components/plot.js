3fd2ce68b84f9c16829de8884db4cf7e40b66198
Matheus Cunha <mathcunha@gmail.com>
2020-04-04 10:41:19 -0300

    Update plot.js

--- pages/components/plot.js
+++ pages/components/plot.js
@@ -13,7 +13,7 @@ class Plot extends Component {
         options={this.props.options}
         series={this.props.series}
         type={this.props.type}
-        width="500"
+        width="450"
         height="380"
       />
     );
